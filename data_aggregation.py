ccfc2f88c06d1440341a136d62ff29bb06cbddd3
Luc Muss <lucas.mussmaecher@gmail.com>
2018-09-30 17:34:37 +0200

    Daten Klasse eingefügt Visualisierung verbessert Outliers wurden entfernt

--- data_aggregation.py
+++ data_aggregation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
 from dataset.water_temperature import WaterTemperature
 from dataset.air_temperature import AirTemperature
@@ -21,111 +22,159 @@ from fishdata.fish_type import FishType
 from fishdata.catch_hour import CatchHour
 from fishdata.catch_month import CatchMonth
 
-# data attributes form the fish database
-catch_date = CatchDate()
-catch_date_series = catch_date.get_series()
-
-fish_type = FishType()
-fish_type_series = fish_type.get_series()
-
-catch_hour = CatchHour()
-catch_hour_series = catch_hour.get_series()
-
-catch_month = CatchMonth()
-catch_month_series = catch_month.get_series()
-
-# data attributes form the environment data
-record_date_hour = RecordDateHour()
-record_date_hour_series = record_date_hour.get_series()
-
-water_temperature = WaterTemperature()
-water_temperature_series = water_temperature.get_series()
-
-air_temperature = AirTemperature()
-air_temperature_series = air_temperature.get_series()
-
-wind_direction = WindDirection()
-wind_direction_series = wind_direction.get_series()
-
-precipitation_amount = PrecipitationAmount()
-precipitation_amount_series = precipitation_amount.get_series()
-
-relative_humidity = RelativeHumidity()
-relative_humidity_series = relative_humidity.get_series()
-
-sun_hours = SunHours()
-sun_hours_series = sun_hours.get_series()
-
-wind_strength = WindStrength()
-wind_strength_series = wind_strength.get_series()
-
-ground_temperature_5 = GroundTemperature5()
-ground_temperature_5_series = ground_temperature_5.get_series()
-
-ground_temperature_10 = GroundTemperature10()
-ground_temperature_10_series = ground_temperature_10.get_series()
-
-ground_temperature_20 = GroundTemperature20()
-ground_temperature_20_series = ground_temperature_20.get_series()
-
-ground_temperature_50 = GroundTemperature50()
-ground_temperature_50_series = ground_temperature_50.get_series()
-
-ground_temperature_100 = GroundTemperature100()
-ground_temperature_100_series = ground_temperature_100.get_series()
-
-data_set = {catch_date.data_name: catch_date_series,
-            fish_type.data_name: fish_type_series,
-            catch_hour.data_name: catch_hour_series,
-            catch_month.data_name: catch_month_series,
-
-            air_temperature.data_name: air_temperature_series,
-            water_temperature.data_name: water_temperature_series,
-            wind_direction.data_name: wind_direction_series,
-            precipitation_amount.data_name: precipitation_amount_series,
-            relative_humidity.data_name: relative_humidity_series,
-            sun_hours.data_name: sun_hours_series,
-            wind_strength.data_name: wind_strength_series,
-            ground_temperature_5.data_name: ground_temperature_5_series,
-            ground_temperature_10.data_name: ground_temperature_10_series,
-            ground_temperature_20.data_name: ground_temperature_20_series,
-            ground_temperature_50.data_name: ground_temperature_50_series,
-            ground_temperature_100.data_name: ground_temperature_100_series,
-            record_date_hour.data_name: record_date_hour_series}
-
-data_frame = pd.DataFrame(data_set)
+from settings import MAXIMAL_SERIES_END_YEAR
+from settings import MINIMAL_SERIES_START_YEAR
 
 
-def get_frame_by_year(year, panda_object):
-    date_start = "{}-01-01 00:00:00".format(year)
-    date_end = "{}-12-31 00:00:00".format(year)
+class FishDataFrame:
+    __data_frame = 0
+    __std_factor = 3.0
+    __data_attribute_names = list()
+    __data_year_dict = dict()
 
-    return panda_object[date_start:date_end]
+    def __init__(self):
+        self.__init_data()
+
+    def __init_data(self):
+        # data attributes form the fish database
+        catch_date = CatchDate()
+        catch_date_series = catch_date.get_series()
+
+        fish_type = FishType()
+        fish_type_series = fish_type.get_series()
+
+        catch_hour = CatchHour()
+        catch_hour_series = catch_hour.get_series()
+
+        catch_month = CatchMonth()
+        catch_month_series = catch_month.get_series()
+
+        # data attributes form the environment data
+        record_date_hour = RecordDateHour()
+        record_date_hour_series = record_date_hour.get_series()
+
+        water_temperature = WaterTemperature()
+        water_temperature_series = water_temperature.get_series()
+
+        air_temperature = AirTemperature()
+        air_temperature_series = air_temperature.get_series()
+
+        wind_direction = WindDirection()
+        wind_direction_series = wind_direction.get_series()
+
+        precipitation_amount = PrecipitationAmount()
+        precipitation_amount_series = precipitation_amount.get_series()
+
+        relative_humidity = RelativeHumidity()
+        relative_humidity_series = relative_humidity.get_series()
+
+        sun_hours = SunHours()
+        sun_hours_series = sun_hours.get_series()
+
+        wind_strength = WindStrength()
+        wind_strength_series = wind_strength.get_series()
+
+        ground_temperature_5 = GroundTemperature5()
+        ground_temperature_5_series = ground_temperature_5.get_series()
+
+        ground_temperature_10 = GroundTemperature10()
+        ground_temperature_10_series = ground_temperature_10.get_series()
+
+        ground_temperature_20 = GroundTemperature20()
+        ground_temperature_20_series = ground_temperature_20.get_series()
+
+        ground_temperature_50 = GroundTemperature50()
+        ground_temperature_50_series = ground_temperature_50.get_series()
+
+        ground_temperature_100 = GroundTemperature100()
+        ground_temperature_100_series = ground_temperature_100.get_series()
+
+        data_set = {catch_date.data_name: catch_date_series,
+                    fish_type.data_name: fish_type_series,
+                    catch_hour.data_name: catch_hour_series,
+                    catch_month.data_name: catch_month_series,
+                    record_date_hour.data_name: record_date_hour_series,
+
+                    air_temperature.data_name: self.__remove_outliers(air_temperature_series),
+                    water_temperature.data_name: self.__remove_outliers(water_temperature_series),
+                    wind_direction.data_name: self.__remove_outliers(wind_direction_series),
+                    precipitation_amount.data_name: self.__remove_outliers(precipitation_amount_series),
+                    relative_humidity.data_name: self.__remove_outliers(relative_humidity_series),
+                    sun_hours.data_name: self.__remove_outliers(sun_hours_series),
+                    wind_strength.data_name: self.__remove_outliers(wind_strength_series),
+                    ground_temperature_5.data_name: self.__remove_outliers(ground_temperature_5_series),
+                    ground_temperature_10.data_name: self.__remove_outliers(ground_temperature_10_series),
+                    ground_temperature_20.data_name: self.__remove_outliers(ground_temperature_20_series),
+                    ground_temperature_50.data_name: self.__remove_outliers(ground_temperature_50_series),
+                    ground_temperature_100.data_name: self.__remove_outliers(ground_temperature_100_series)
+                    }
+
+        self.__data_frame = pd.DataFrame(data_set)
+
+        self.__data_attribute_names = self.__get_data_attributes(self.__data_frame)
+        self.__data_attribute_names_plot = self.__get_plotable_attributes(self.__data_frame)
+
+    @property
+    def plotable_attributes(self):
+        return self.__data_attribute_names_plot
+
+    @property
+    def data_attributes(self):
+        return self.__data_attribute_names
+
+    def __get_data_attributes(self, data_frame):
+        return list(data_frame.keys())
+
+    def __get_plotable_attributes(self, data_frame):
+        return_list = [name for name in data_frame.columns if (data_frame[name].dtype == 'float64')]
+        return return_list
+
+    def get_frame_by_year(self, year):
+        date_start = "{}-01-01 00:00:00".format(year)
+        date_end = "{}-12-31 00:00:00".format(year)
+
+        return self.__data_frame[date_start:date_end]
+
+    def __remove_outliers(self, panda_series):
+        mean_value = panda_series.mean()
+        std_value = panda_series.std()
+        compare_factor = self.__std_factor * std_value
+
+        bad_index = panda_series[np.abs(panda_series - mean_value) >= compare_factor]
+        panda_series = panda_series.drop(index=bad_index.index)
+
+        return panda_series
+
+    def __get_year_list(self):
+        date_time_start = datetime.datetime.strptime(MINIMAL_SERIES_START_YEAR, "%Y-%m-%d %H:00:00")
+        year_start = date_time_start.strftime("%Y")
+        year_start_int = int(year_start)
+
+        date_time_end = datetime.datetime.strptime(MAXIMAL_SERIES_END_YEAR, "%Y-%m-%d %H:00:00")
+        year_end = date_time_end.strftime("%Y")
+        year_end_int = int(year_end)
+
+        year_range = range(year_start_int, year_end_int + 1)
+        year_return_list = [str(year) for year in year_range]
+
+        return year_return_list
+
+    def get_year_dict(self):
+        for year in self.__get_year_list():
+            self.__data_year_dict[year] = self.get_frame_by_year(year)
+
+        self.__data_year_dict['*'] = self.__data_frame
+
+        return self.__data_year_dict
 
 
-def remove_outliers(std_factor, panda_object):
-    mean_value = panda_object.mean()
-    std_value = panda_object.std()
-    compare_factor = std_factor * std_value
+fish_data = FishDataFrame()
 
-    bad_index = panda_object[np.abs(panda_object - mean_value) >= compare_factor]
-    panda_object = panda_object.drop(index=bad_index.index)
-
-    return panda_object
-
-
-def print_hist(panda_object):
-    data_values = panda_object.get_values()
-    plt.hist(data_values)
-
-
-data_year_2017 = get_frame_by_year('2017', data_frame)
-air_temperature_year_2017 = data_year_2017['air_temperature']
-
-air_temperature_year_2017_filterd = remove_outliers(3, air_temperature_year_2017)
+# air_temperature_year_2017 = fish_year_dict['2017']['air_temperature']
+# air_temperature_year_2017_filtered = fish_data.remove_outliers(air_temperature_year_2017)
 
 # data_temp_hoch = data_year_2017.query('air_temperature > 28')
 # data_temp_hoch.plot.line()
 
-x_values = list(air_temperature_year_2017_filterd.index.get_values())
-y_values = list(air_temperature_year_2017_filterd)
+# x_values = list(air_temperature_year_2017_filtered.index.get_values())
+# y_values = list(air_temperature_year_2017_filtered)
--- data_visualization.py
+++ data_visualization.py
@@ -1,29 +1,90 @@
 import dash
+import flask
+import os
+
 import dash_core_components as dcc
 import dash_html_components as html
+from flask import send_from_directory
 
-from data_aggregation import x_values, y_values
+from data_aggregation import fish_data
 
-app = dash.Dash()
+custom_stylesheet = ['/static/custom_style.css']
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+server = flask.Flask(__name__)
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+app = dash.Dash(__name__, server=server, external_stylesheets=custom_stylesheet)
 
-    dcc.Graph(
-        id='example-graph',
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
+
+text_description = '''The data set was collected form different kinds 
+of fish catches in the river Baunach.'''
+
+
+@app.server.route('/static/<path:path>')
+def static_file(path):
+    static_folder = os.path.join(os.getcwd(), 'static')
+    return send_from_directory(static_folder, path)
+
+
+def generate_graphs(fish_data):
+    fish_year_dict = fish_data.get_year_dict()
+
+    # data_2017_air_temperature = fish_year_dict['2017']['air_temperature']
+    data_2016_water_temperature = fish_year_dict['2016']['water_temperature']
+
+    # graph_2017 = graph_by_series(data_2017_air_temperature, '2017', 'air_temperature')
+    graph_2016 = graph_by_series(data_2016_water_temperature, '2016', 'water_temperature')
+
+    return graph_2016
+
+
+def graph_by_series(data_series, year, attribute):
+    data_series = data_series
+
+    x_values = list(data_series.index.get_values())
+    y_values = list(data_series.get_values())
+
+    graph_id = "{}_{}".format(year, attribute)
+    graph_name = "{}".format(attribute)
+
+    attribute_title = attribute.title().replace('_', ' ')
+    year_title = year.title()
+
+    graph_title = "{} {}".format(attribute_title, year_title)
+
+    new_graph = dcc.Graph(
+        id=graph_id,
+
         figure={
             'data': [
-                {'x': x_values, 'y': y_values, 'type': 'line', 'name': 'SF'},
+                {'x': x_values, 'y': y_values, 'type': 'line', 'name': graph_name},
             ],
             'layout': {
-                'title': 'Dash Data Visualization'
+                'title': graph_title
             }
         }
     )
+    return new_graph
+
+
+app.layout = html.Div(children=[
+
+    html.Link(
+        rel='stylesheet',
+        href='/static/custom_style.css'
+    ),
+
+    html.H2(children='Visualization of Environmental Data of the River Baunach',
+            style={'className': 'superclass', 'textAlign': 'center'}),
+
+    dcc.Markdown(children=text_description),
+
+    html.Div(children=[
+
+        generate_graphs(fish_data)
+
+    ])
 ])
 
 if __name__ == '__main__':
